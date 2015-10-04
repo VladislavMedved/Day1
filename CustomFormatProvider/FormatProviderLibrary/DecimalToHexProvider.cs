39df7103d4464775cd3d409a2d49da9e1b12aee4
Vladislav Medved <medved.vladislav@gmail.com>
2015-10-04 21:19:50 +0300

    Create decimal to hex integer provider

---
+++ CustomFormatProvider/FormatProviderLibrary/DecimalToHexProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatProviderLibrary
+{
+    public class DecimalToHexProvider: IFormatProvider, ICustomFormatter
+    {
+        static readonly string[] hexCharacters = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        IFormatProvider parent;
+        public DecimalToHexProvider() : this(CultureInfo.CurrentCulture) { }
+        public DecimalToHexProvider(IFormatProvider provider)
+        {
+            parent = provider;
+        }
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter)) return this;
+            return null;
+        }
+        public string Format(string format, object arg, IFormatProvider provider)
+        {
+            if (arg == null || format != "Hx")
+                return String.Format(parent,"{0:" + format + "}", arg);
+            StringBuilder hexString = new StringBuilder();
+            Stack<int> stack = new Stack<int>();
+            int popped,delta, dec, providerBase = 16;
+            try
+            {
+                dec = (int)arg;
+            }
+            catch (InvalidCastException ex)
+            {
+                
+                throw new InvalidCastException("DecimalToHexProvider must recieve numerical type",ex);
+            }
+            
+            while (dec != 0)
+            {
+                stack.Push(dec);
+                dec /= providerBase;
+            }
+            delta = popped = stack.Pop();
+            hexString.Append(hexCharacters[delta]);
+            while (stack.Count > 0)
+            {
+                
+                delta = stack.Peek() - popped * providerBase;
+                hexString.Append(hexCharacters[delta]);
+                popped = stack.Pop();
+                
+            }
+            return hexString.ToString();
+        }
+    }
+}
---
+++ CustomFormatProvider/FormatProviderLibraryTests/HexFormatProviderTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormatProviderLibrary;
+
+namespace FormatProviderLibraryTests
+{
+    [TestClass]
+    public class HexFormatProviderTest
+    {
+        [TestMethod]
+        public void HexFormatter_WithArgumentEquals423_MustReturn1A7()
+        {
+            DecimalToHexTest(423, "1A7");
+        }
+
+        [TestMethod]
+        public void HexFormatter_WithArgumentEquals163522_MustReturn27EC2()
+        {
+            DecimalToHexTest(163522, "27EC2");
+        }
+
+        [TestMethod]
+        public void HexFormatter_WithArgumentEquals1027_MustReturn403()
+        {
+            DecimalToHexTest(1027, "403");
+        }
+
+        [TestMethod]
+        public void HexFormatter_WithWrongFormatString_DoesntReturnHex()
+        {
+            int dec = 123;
+            IFormatProvider provider = new DecimalToHexProvider();
+            string expected = "00123";
+            string actual = String.Format(provider, "{0:D5}", dec);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void HexFormatter_WithStringArgument_ThrownException()
+        {
+            IFormatProvider provider = new DecimalToHexProvider();
+            string actual = String.Format(provider, "{0:Hx}", "00123");
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void HexFormatter_WithDoubleArgument_ThrownException()
+        {
+            IFormatProvider provider = new DecimalToHexProvider();
+            string actual = String.Format(provider, "{0:Hx}", 123.552);
+            Assert.Fail();
+        }
+
+        private void DecimalToHexTest(int dec, string expected)
+        {
+            IFormatProvider provider = new DecimalToHexProvider();
+            string actual = String.Format(provider, "{0:Hx}", dec);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
